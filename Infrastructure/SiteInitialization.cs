bed986118f40e29d7f92d505e060033b261282a4
kazirahiv <rahiv.mail@gmail.com>
2022-03-30 19:54:00 +0600

    Init

---
+++ Infrastructure/SiteInitialization.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Commerce.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace EpiCommerce.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+    public class SiteInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            CatalogRouteHelper.MapDefaultHierarchialRouter(false);
+        }
+
+        public void Preload(string[] parameters) { }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,86 @@
+﻿using EPiServer.Cms.UI.AspNetIdentity;
+using EPiServer.Data;
+using EPiServer.Web;
+using EPiServer.Web.Routing;
+using Mediachase.Commerce.Anonymous;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace EpiCommerce
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _webHostingEnvironment;
+        private readonly IConfiguration _configuration;
+
+        public Startup(IWebHostEnvironment webHostingEnvironment, IConfiguration configuration)
+        {
+            _webHostingEnvironment = webHostingEnvironment;
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionStringCommerce = _configuration.GetConnectionString("EcfSqlConnection");
+
+            services.Configure<DataAccessOptions>(o =>
+            {
+                o.ConnectionStrings.Add(new ConnectionStringOptions
+                {
+                    ConnectionString = connectionStringCommerce,
+                    Name = "EcfSqlConnection"
+                });
+            });
+
+            services.AddCmsAspNetIdentity<ApplicationUser>(o =>
+            {
+                if (string.IsNullOrEmpty(o.ConnectionStringOptions?.ConnectionString))
+                {
+                    o.ConnectionStringOptions = new ConnectionStringOptions()
+                    {
+                        Name = "EcfSqlConnection",
+                        ConnectionString = connectionStringCommerce
+                    };
+                }
+            });
+
+            if (_webHostingEnvironment.IsDevelopment())
+            {
+                //Add development configuration
+            }
+
+            services.AddMvc();
+            services.AddCommerce();
+            services.AddEmbeddedLocalization<Startup>();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/util/Login";
+            });
+            services.TryAddEnumerable(ServiceDescriptor.Singleton(typeof(IFirstRequestInitializer), typeof(UsersInstaller)));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAnonymousId();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "Default", pattern: "{controller}/{action}/{id?}");
+                endpoints.MapContent();
+            });
+        }
+    }
+}
